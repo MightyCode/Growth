59784041ba37c409c60a0904dec4ed5457fa528c
MightyCode <bazinmaxence@free.fr>
2018-05-09 22:04:32 +0200

    Changement du nom du package.

--- src/migthycode/growth/game/utils/Math.java
+++
@@ -1,7 +0,0 @@
-package migthycode.growth.game.utils;
-
-public abstract class Math {
-	public static double map(double x, double a, double b, double c, double d) {
-		return (x - a) / (b - a) * (d - c) + c;
-	}
-}
