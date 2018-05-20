54e1770eabfd99683fdc3b564b703f2dc3603f2a
MaxBazin <36881945+MaxBazin@users.noreply.github.com>
2018-05-20 17:04:18 +0200

    Update Growth.java
    Nouvelle version : 0.2.3 en cours de développement !

--- src/growth/main/Growth.java
+++ src/growth/main/Growth.java
@@ -4,7 +4,7 @@ package growth.main;
  * Main class of the game.
  *
  * @author MightyCode
- * @version of game : 2.2.2
+ * @version of the current game develloped: 0.2.3
  */
 public class Growth {
 
