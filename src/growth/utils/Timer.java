12e24556fc299b1b21d3bb871865c6eabdf623a6
yoctoctet <yoctoctet@gmail.com>
2018-07-06 16:18:09 +0200

    Création de la classe timer.

---
+++ src/growth/utils/Timer.java
@@ -0,0 +1,37 @@
+package growth.utils;
+
+public class Timer {
+    /**
+     * Time of activation.
+     */
+    private long startTime;
+
+    public Timer() {
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Reset Timer.
+     */
+    public void reset() {
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Get the time between startTime and now.
+     *
+     * @return startTime - now
+     */
+    public long getDuration() {
+        return System.nanoTime() - startTime;
+    }
+
+    /**
+     * Get time of activation.
+     *
+     * @return startTime
+     */
+    public long getStartTime() {
+        return startTime;
+    }
+}
